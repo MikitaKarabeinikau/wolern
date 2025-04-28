4da86bce1f9127d9506f03e64877ea05c496f8de
Nikita Karabeinikau <nikitakarabeinikau@gmail.com>
2025-04-28 20:52:20 +0200

    feat(vocabulary): add get_list_of_words

--- src/vocabulary.py
+++ src/vocabulary.py
@@ -80,7 +80,8 @@ def add_word_to_vocabulary(word,vocabulary=vocab):
     vocab[word["word"]] = word
     VOCABULARY_PATH.write_text(json.dumps(vocab,ensure_ascii=False,indent=2),encoding="utf-8")
 
-
+def get_list_of_words(vocabulary):
+    return list(vocabulary.keys())
 
 def show_vocabulary_list(vocabulary):
     return '\n'.join(list(vocabulary.keys()))
