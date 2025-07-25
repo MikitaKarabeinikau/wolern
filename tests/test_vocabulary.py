c1afd4b6b6ac63a4c4eb63521e1fbac5bcae000b
MikitaKarabeinikau <nikitakarabeinikau@gmail.com>
2025-07-25 16:04:48 +0200

    a

--- tests/test_vocabulary.py
+++ tests/test_vocabulary.py
@@ -3,19 +3,6 @@ from pathlib import Path
 import pytest
 from src.vocabulary import *
 
-
-def test_is_word_in_vocabulary_True():
-    assert is_word_in_vocabulary('dog',Path(__file__).resolve().parent.parent/'tests'/'data'/'test_vocabulary.json') == True
-def test_is_word_in_vocabulary_False():
-    assert is_word_in_vocabulary('mod',Path(__file__).resolve().parent.parent/'tests'/'data'/'test_vocabulary.json') == False
-
-def test_is_word_in_vocabulary_None():
-    with pytest.raises(ValueError,match="Word should not be None"):
-        assert is_word_in_vocabulary(None,Path(__file__).resolve().parent.parent/'tests'/'data'/'test_vocabulary.json') == False
-
-def test_is_word_in_vocabulary_TO_SHORT_WORD():
-    with pytest.raises(ValueError,match='Word should have more symbols then 1'):
-        assert is_word_in_vocabulary('b',Path(__file__).resolve().parent.parent/'tests'/'data'/'test_vocabulary.json') == False
 @pytest.fixture
 def vocabulary():
     return Vocabulary()
