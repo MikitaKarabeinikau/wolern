2d159ca364d63eb9aea7106df3d3dd16b9107a29
Mikita Karabeinikau <123440849+MikitaKarabeinikau@users.noreply.github.com>
2025-07-25 15:31:57 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wolern', 
+    version='0.0.1', # Start with a version number
+    packages=find_packages(),
+    install_requires=[],
+    author='Mikita Karabeinikau',
+    description='Coming soon...',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/MikitaKarabeinikau/wolern', 
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License', # Or whatever license you use
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8', 
+)
